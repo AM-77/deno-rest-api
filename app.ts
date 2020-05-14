b505771c34f22f50f57d233dee13b0994ff29d4f
AM-77 <amine.griche77@gmail.com>
2020-05-14 17:15:31 +0200

    add app.ts

--- app.ts
+++ app.ts
@@ -1,13 +1,14 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+import router from './router.ts'
 
-const env:{PORT: number, HOST: string} = Deno.env.toObject()
-const HOST:string = env.HOST || '127.0.0.1'
-const PORT:number = env.PORT || 7700
-
-const router = new Router()
+const env = Deno.env.toObject()
+const HOST = env.HOST || '127.0.0.1'
+const PORT = env.PORT || 7700
 
 const app = new Application()
+
 app.use(router.routes())
 app.use(router.allowedMethods())
-console.log(`Listening on port ${PORT}...`)
+
+console.log(`Listening on port ${PORT} ...`)
 await app.listen(`${HOST}:${PORT}`)
